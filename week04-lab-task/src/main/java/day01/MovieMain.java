f9c4541a137291cb368db67e0d33018e48eb5f5a
gkerepeszky <gkerepeszky@ofnet.hu>
2021-11-02 18:08:42 +0100

    NEW

--- week04-lab-task/src/main/java/day01/MovieMain.java
+++ week04-lab-task/src/main/java/day01/MovieMain.java
@@ -5,8 +5,11 @@ public class MovieMain {
     public static void main(String[] args) {
         Movie movie = new Movie("Titanic", 1997);
 
-        movie.addActor(new Actor("Dicaprio", 1968));
-        movie.addActor(new Actor("Dicaprio", 1978));
+        movie.addActor(new Actor("Leonardo DiCaprio", 1975));
+        movie.addActor(new Actor("Kate Winslet", 1978));
+        movie.addActor(new Actor("Billy Zane", 1966));
+        movie.addActor(new Actor("Danny Nucci", 1968));
+
         System.out.println("Színészek a huszas éveikben:" + movie.actorsInTheirTwenties());
 
 
